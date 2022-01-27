2e6d129309bff6c8d575a8faca461fc9ee8b134e
Sabinastorm <sabinasaila@icloud.com>
2022-01-27 14:22:14 +0000

    Add style to buttons

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const restartBtn = document.getElementById('');
+const prevBtn = document.getElementById('');
+const nextBtn = document.getElementById('');
+const submitBtn= document.getElementById('');
+const trueBtn = document.getElementById('');
+const falseBtn= document.getElementById('');
+const userScore = document.getElementById('');
+const questionText = document.getElementById('');
