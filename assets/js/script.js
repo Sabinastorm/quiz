0cd083079f85ca8d3b43635056cf51bac18f0e36
Sabinastorm <sabinasaila@icloud.com>
2022-02-01 07:20:07 +0000

    Add style quiz-area

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,14 @@ const restartBtn = document.getElementById('restart');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 const submitBtn= document.getElementById('submit');
-const trueBtn = document.getElementById('True');
-const falseBtn= document.getElementById('False');
+const trueBtn = document.getElementById('true');
+const falseBtn= document.getElementById('false');
 const userScore = document.getElementById('user-score');
 const questionText = document.getElementById('question-text');
-
+let totalScoreText = document.getElementById('total-score');
 let currentQuestion = 0;
 var score = 0;
-
-let question = [
+let questions = [
     {
         question: "What flightless bird went extinct in the 1660s and has a reputation for stupidity?",
         answers: [
@@ -20,7 +19,7 @@ let question = [
     },
     {
         question: "According to the Big Bang Theory, how did the universe begin?",
-        answer: [
+        answers: [
             {option:"A meteor shower",answer:false},
             {option:"An explosion",answer:true}
         ]
@@ -31,14 +30,19 @@ let question = [
             {option:"Troy",answer:true},
             {option:"Persia",answer:false}
         ]
+    },
+    {
+      question: "Which Chess pieces moves first?",
+      answers: [
+          {option:"White",answer:true},
+          {option:"Black",answer:false}
+      ]
     }
 ]
-
 restartBtn.addEventListener('click',restart);
 prevBtn.addEventListener('click',prev);
 nextBtn.addEventListener('click',next);
 submitBtn.addEventListener('click',submit);
-
 function beginQuiz(){
     currentQuestion = 0;
     questionText.innerHTML = questions[currentQuestion].question;
@@ -55,7 +59,7 @@ function beginQuiz(){
             next();
         }
     }
-    falseBtn.innerHTML = questions[currentQuestion].answer[1].option;
+    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
     falseBtn.onclick = () => {
         let ano=1;
         if(questions[currentQuestion].answers[ano].answer){
@@ -70,7 +74,6 @@ function beginQuiz(){
     }
     prevBtn.classList.add('hide');
 }
-
 function restart(){
     currentQuestion = 0;
     prevBtn.classList.remove('hide');
@@ -82,9 +85,13 @@ function restart(){
     userScore.innerHTML = score;
     beginQuiz();
 }
-
 function next(){
     currentQuestion++;
+    console.log(currentQuestion);
+    console.log(numberOfQuestions);
+    if (currentQuestion === numberOfQuestions) {
+      submit();
+    }
     if(currentQuestion>=2){
         nextBtn.classList.add('hide');
         prevBtn.classList.remove('hide');
@@ -94,31 +101,30 @@ function next(){
     trueBtn.onclick = () => {
         let ano=0;
         if(questions[currentQuestion].answers[ano].answer){
-            if(score<3){
+            if(score<numberOfQuestions){
                 score++; 
             }
         }
         userScore.innerHTML = score;
-        if(currentQuestion<2){
+        if(currentQuestion<numberOfQuestions){
             next();
         }
     }
-    falseBtn.innerHTML = question[currentQuestion].answers[1].option;
+    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
     falseBtn.onclick = () => {
         let ano=1;
         if(questions[currentQuestion].answers[ano].answer){
-            if(score<3){
+            if(score<numberOfQuestions){
                 score++;
             }
         }
         userScore.innerHTML = score;
-        if(currentQuestion<2){
+        if(currentQuestion<numberOfQuestions){
             next();
         }
     }
     prevBtn.classList.remove('hide');
 }    
-
 function prev(){
     currentQuestion--;
     if(currentQustion<=0){
@@ -154,12 +160,14 @@ function prev(){
     }
     nextBtn.classList.remove('hide');
 }    
-
 function submit(){
     prevBtn.classList.add('hide');
     nextBtn.classList.add('hide');
     submitBtn.classList.add('hide');
     trueBtn.classList.add('hide');
-    falseBtn.classList.add('hide'); 
+    falseBtn.classList.add('hide');
     questionText.innerHTML = "Congratulations!";
 }
+beginQuiz();
+const numberOfQuestions = questions.length;
+totalScoreText.innerText = `/ ${numberOfQuestions}`;
